c6dd90d6402e0365a82017ce45a1c345302aa7e5
KalyanArun <kalyanarunachalam@gmail.com>
2023-01-20 18:46:48 +0530

    git commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const navbar = (
+  <nav>
+    <h1>AK</h1>
+    <ul>
+      <li>Menu</li>
+      <li>Home</li>
+      <li>Templates</li>
+      <li>Login</li>
+    </ul>
+  </nav>
+);
+
+ReactDOM.render(navbar, document.getElementById("root"));
